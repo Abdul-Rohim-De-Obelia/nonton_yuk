fae428a4218d5065f774ff81d569d08ba2533dcf
Rohimm <limitproz@gmail.com>
2026-06-24 13:57:14 +0700

    Update LoadingSpinner.vue

--- src/components/common/LoadingSpinner.vue
+++ src/components/common/LoadingSpinner.vue
@@ -1,4 +1,4 @@
-<!-- File ini diletakkan di: src/components/common/LoadingSpinner.vue -->
+
 <template>
   <div class="flex justify-center items-center py-12">
     <div class="w-10 h-10 border-4 border-accent/30 border-t-accent rounded-full animate-spin"></div>
